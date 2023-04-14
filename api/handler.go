53a9074b6599a290f1d39abd700cfac3dfc2e874
Hadi <hadiforooghi1377@gmail.com>
2023-04-15 00:30:18 +0330

    body handler

--- api/handler.go
+++ api/handler.go
@@ -1,10 +1,16 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func getBody(c echo.Context) error  {
-	return c.String(http.StatusOK, "get body")
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	return c.JSON(http.StatusOK, json_map)
 }
